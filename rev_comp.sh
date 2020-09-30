f6dd1d31660a9c92d2b68d499699e3a8b32e2fa3
eva <rza0045@auburn.edu>
2020-09-30 18:21:10 -0500

    If else statement

---
+++ rev_comp.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+if [ $# -lt 1 ] || [ $# -gt 1 ]
+then
+#initialize variable
+reverse=""
+
+#read in data
+name=`grep ">" $1`
+sequence=`grep -v ">" $1`
+
+echo "Name: $name"
+
+#reverse sequence with command rev
+
+reverse=`rev $sequence`
+
+echo "$sequence
+$reverse
+"
+
+#complement sequence
+rc=`echo $reverse | tr 'atcgATCG' 'tagcTAGC'`
+
+echo "$rc
+"
+
+echo $name >$1.rc.txt
+echo $rc >>$1.rc.txt
+<<<<<<< HEAD
+=======
+# add reflection 
+
+>>>>>>> 08e9301c66b745a318cb3c2d161da58e5f8edc9c
+else
+echo 'incorrect  no. of argument'
+done
+
